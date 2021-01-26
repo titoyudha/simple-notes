02d646a7aa8cf4269b346a53ce716fedb701ff9f
Tito Yudha <tito.yudha.bimashika@gmail.com>
2021-01-27 00:59:29 +0700

    add css

--- src/index.js
+++ src/index.js
@@ -8,12 +8,7 @@ import firebase from './config/firebase';
 console.log('config firebase ===>',firebase);
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
